f256f900e319ee06ab7b7666ecab2dda372467b5
eduwr <eduwric@gmail.com>
2019-12-11 00:31:07 -0300

    gráfico gerado - sem estilo

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import BarChart from '../charts/chart';
 
+
 export default function Main() {
     return (
-        <div className="chart" style={{height: '80vh'}}>
-            <BarChart />
+        <div className="wrapper">
+            <h1>Brazil GDP</h1>
+            <div className="chart" style={{height: '80vh'}}>
+                <BarChart />
+            </div>
         </div>
     )
 }
